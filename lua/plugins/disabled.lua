de1bf99a57562183e31aa85e3290901a46e8e45e
SealCia1123 <maoduchai404@gmail.com>
2024-06-05 08:38:42 +0700

    disable neotab and add tabout

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
     -- disable trouble
     { "akinsho/bufferline.nvim", enabled = false },
     -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
+    { "kawre/neotab.nvim", enabled = false },
 }
